d92981b93748e86fefd4e3d8e81a6babe081341a
yaooo <yaoshi616@gmail.com>
2019-02-19 12:14:00 -0500

    fix button issues

--- TextToSpeech/app/src/main/java/com/example/zchao/texttospeech/MainActivity.java
+++ TextToSpeech/app/src/main/java/com/example/zchao/texttospeech/MainActivity.java
@@ -24,6 +24,7 @@ import android.os.Bundle;
  */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import android.os.Bundle;
@@ -55,6 +56,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
         //初始化TTS
         tts = new TextToSpeech(this,new TextToSpeech.OnInitListener(){
             @Override
@@ -62,7 +67,7 @@ public class MainActivity extends Activity {
                 // 判断是否转化成功
                 if (status == TextToSpeech.SUCCESS) {
                     //默认设定语言为中文
-                    int result = tts.setLanguage(Locale.CHINESE);
+                    int result = tts.setLanguage(Locale.ENGLISH);
                     if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                         Log.e("tts", "language not supported");
                     } else {
@@ -74,28 +79,29 @@ public class MainActivity extends Activity {
         });
         //获取控件
         speechText = findViewById(R.id.edit_text);
+        speechText.setFocusable(false); // disable editing
         speechButton = findViewById(R.id.button_speak);
         show = findViewById(R.id.listView);
-        save = findViewById(R.id.save_forlater);
-        save.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String getInput = speechText.getText().toString();
-
-                if(addArray.contains(getInput)){
-                    Toast.makeText(getBaseContext(),"Added",Toast.LENGTH_LONG).show();
-                }
-                else if (getInput == null || getInput.trim().equals("")) {
-                    Toast.makeText(getBaseContext(),"Empty",Toast.LENGTH_LONG).show();
-                }
-                else{
-                    addArray.add(getInput);
-                    ArrayAdapter<String>adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,addArray);
-                    show.setAdapter(adapter);
-                    ((EditText)findViewById(R.id.edit_text)).setText(" ");
-                }
-            }
-        });
+//        save = findViewById(R.id.save_forlater);
+//        save.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String getInput = speechText.getText().toString();
+//
+//                if(addArray.contains(getInput)){
+//                    Toast.makeText(getBaseContext(),"Added",Toast.LENGTH_LONG).show();
+//                }
+//                else if (getInput == null || getInput.trim().equals("")) {
+//                    Toast.makeText(getBaseContext(),"Empty",Toast.LENGTH_LONG).show();
+//                }
+//                else{
+//                    addArray.add(getInput);
+//                    ArrayAdapter<String>adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,addArray);
+//                    show.setAdapter(adapter);
+//                    ((EditText)findViewById(R.id.edit_text)).setText(" ");
+//                }
+//            }
+//        });
         //为button添加监听
 
 
@@ -110,16 +116,13 @@ public class MainActivity extends Activity {
     }
 
     private void loop(){
-        int i = 0;
-        while (i < 3){
-            String text = addArray.get(i);
-            tts.speak(text,TextToSpeech.QUEUE_FLUSH, null);
-            try {
-                Thread.sleep(1000);
-            } catch(InterruptedException e) {
-                System.out.println("got interrupted!");
-            }
-            i++;
+        String test = "There are many problems associated with training dogs: the cost of training guide dog is high; not every dog can be a guide dog; the average training time is about one year; a guide dog requires care, food and love; a guide dog is only able to serve for about 12 years; a guide dog cannot guide people to a specific address. Therefore, we come up with the idea of Mobile Voice Assistant aiming to substitute the guide dogs.";
+        //String[] blocks = test.split(" ");
+        //addArray.addAll(Arrays.asList(blocks));
+        addArray.add(test);
+        for (String i : addArray) {
+            tts.speak(i,TextToSpeech.QUEUE_ADD, null);
+            tts.playSilence(10, TextToSpeech.QUEUE_ADD, null);
         }
 
     }
